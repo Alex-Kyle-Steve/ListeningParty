19de4862143da395a2ae24b623796fa7f0d02e60
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-03-19 21:52:03 -0500

    add condition to prevent infinite loop

--- client/socket.js
+++ client/socket.js
@@ -13,29 +13,29 @@ socket.on('connect', () => {
   })
 })
 
-socket.on('recieve_new_uri', uri => {
-  // uri =>
-  //   window.player.getCurrentState().then(state => {
-  //     if (state.track_window.current_track.uri !== uri)
-  window.player._options.getOAuthToken(accessToken =>
-    fetch(
-      `https://api.spotify.com/v1/me/player/play?device_id=${
-        window.player._options.id
-      }`,
-      {
-        method: 'PUT',
-        body: JSON.stringify({uris: [uri]}),
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`
-        }
-      }
-    )
-      .then(console.log)
-      .catch(console.error)
-  )
-})
-//   })
-// )
+socket.on('recieve_new_uri', uri =>
+  window.player.getCurrentState().then(state => {
+    // if state is null, no music is playing. WHY SPOTIFY!!!!!!!!
+    // just play the song if current track does not match the owner's song
+    if (!state || state.track_window.current_track.uri !== uri)
+      window.player._options.getOAuthToken(accessToken =>
+        fetch(
+          `https://api.spotify.com/v1/me/player/play?device_id=${
+            window.player._options.id
+          }`,
+          {
+            method: 'PUT',
+            body: JSON.stringify({uris: [uri]}),
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${accessToken}`
+            }
+          }
+        )
+          .then(console.log)
+          .catch(console.error)
+      )
+  })
+)
 
 export default socket
