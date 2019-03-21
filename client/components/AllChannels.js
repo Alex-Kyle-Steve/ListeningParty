2153f72a61d1af7c28e18037f2f3e828e2265fad
Kyle <43893339+KyleLee95@users.noreply.github.com>
2019-03-21 12:07:55 -0500

    Styling homepage (#33)
    * homepage looks like less terrible dogshit
    
    * homepage is styled. doesn't go to edge of the window for some reason.

--- client/components/AllChannels.js
+++ client/components/AllChannels.js
@@ -1,54 +1,49 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {Card, Container, Row, Col} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import {Card, Container, Col, ListGroup} from 'react-bootstrap'
 import {fetchChannels} from '../store/channel'
+import {ConnectedChannelLineItem} from './ChannelLineItem'
 
 export class AllChannels extends Component {
-  constructor() {
-    super()
+  constructor(props, context) {
+    super(props, context)
   }
-
   async componentDidMount() {
     await this.props.fetchChannels()
   }
 
   render() {
     const channels = this.props.allChannels
+
     return (
       <Container>
-        <h3>All Channels</h3>
-        {channels && channels.length ? (
-          channels.map(channel => (
-            <Row key={channel.id} md={4}>
-              <Card>
-                <Card.Body>
-                  <Card.Text>
-                    <Link to={`/channels/${channel.id}`}>{channel.name}</Link>
-                  </Card.Text>
-                </Card.Body>
+        <ListGroup>
+          <h3>All Channels</h3>
+          {channels && channels.length ? (
+            channels.map(channel => {
+              return (
+                <ConnectedChannelLineItem key={channel.id} channel={channel} />
+              )
+            })
+          ) : (
+            <Col xs={12}>
+              <Card border="light">
+                <Card.Text className="center">No Channels Found</Card.Text>
               </Card>
-              <br />
-            </Row>
-          ))
-        ) : (
-          <Col xs={12}>
-            <Card border="light">
-              <Card.Text className="center">
-                <h2>No favorite channels</h2>
-              </Card.Text>
-            </Card>
-          </Col>
-        )}
+            </Col>
+          )}
+        </ListGroup>
       </Container>
     )
   }
 }
+
 const mapDispatchToProps = dispatch => {
   return {
     fetchChannels: () => dispatch(fetchChannels())
   }
 }
+
 const mapStateToProps = state => {
   return {
     allChannels: state.channel.allChannels
