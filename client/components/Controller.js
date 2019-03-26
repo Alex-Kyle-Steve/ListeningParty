96e2b0a1a73e31187d4da749c67066c6ca0b31a9
Kyle Lee <Kylecrlee95@gmail.com>
2019-03-26 14:31:45 -0500

    implemented spotify API functionality to controller. this.props.playlist comes in as undefined but otherwise the player works

--- client/components/Controller.js
+++ client/components/Controller.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import axios from 'axios'
 export class Controller extends Component {
   constructor() {
     super()
@@ -6,12 +8,75 @@ export class Controller extends Component {
       togglePlay: false,
       sliderValue: 0
     }
-    this.togglePlayButton = this.togglePlayButton.bind(this)
+    this.togglePlay = this.togglePlay.bind(this)
+    this.togglePause = this.togglePause.bind(this)
+    this.toggleSkip = this.toggleSkip.bind(this)
+    this.togglePrev = this.togglePrev.bind(this)
+    this.seekTrack = this.seekTrack.bind(this)
   }
 
-  togglePlayButton() {
+  async togglePlay() {
     this.setState({
       togglePlay: !this.state.togglePlay
+    })
+    await axios({
+      method: 'put',
+      url: `https://api.spotify.com/v1/me/player/play?device_id=${
+        this.props.player._options.id
+      }`,
+      headers: {
+        Authorization: `Bearer ${this.props.user.accessToken}`
+      },
+      data: {
+        uris: this.props.playlist
+      }
+    })
+  }
+
+  async togglePause() {
+    this.setState({
+      togglePlay: !this.state.togglePlay
+    })
+    await axios({
+      method: 'put',
+      url: `https://api.spotify.com/v1/me/player/pause`,
+      headers: {
+        Authorization: `Bearer ${this.props.user.accessToken}`
+      }
+    })
+  }
+
+  async toggleSkip() {
+    await axios({
+      method: 'post',
+      url: `https://api.spotify.com/v1/me/player/next`,
+      headers: {
+        Authorization: `Bearer ${this.props.user.accessToken}`
+      }
+    })
+  }
+
+  async togglePrev() {
+    await axios({
+      method: 'post',
+      url: `https://api.spotify.com/v1/me/player/previous`,
+      headers: {
+        Authorization: `Bearer ${this.props.user.accessToken}`
+      }
+    })
+  }
+
+  async seekTrack() {
+    await axios({
+      method: 'put',
+      url: 'https://api.spotify.com/v1/me/player/seek',
+      headers: {
+        Authorization: `Bearer ${this.props.user.accessToken}`
+      }
+      // TODO: show position in MS
+      // data:{
+      //   position_ms:
+      // }
     })
   }
 
@@ -19,33 +84,28 @@ export class Controller extends Component {
     //TODO:
     //Create function that increments the sliderValue as a function of the duration of the song.
     //Have dynamic scrubbing
-    var input = document.getElementById('myRange').step
-    while (currentPosition < songLength) {
-      this.onInput(currentPosition)
-      currentPosition++
-    }
-    console.log(input.value)
   }
   //currentPosition and songLength are values in Milliseconds
 
   render() {
+    console.log(this.props)
     return (
       <div id="player-container">
         <div id="player-controls">
           <div className="row right">
-            <i className="fa fa-step-backward">
+            <i onClick={this.togglePrev} className="fa fa-step-backward">
               <img src="/back.png" />
             </i>
             {this.state.togglePlay === false ? (
-              <i onClick={this.togglePlayButton} className="fa fa-pause-circle">
+              <i onClick={this.togglePlay} className="fa fa-pause-circle">
                 <img src="/play.png" />
               </i>
             ) : (
-              <i onClick={this.togglePlayButton} className="fa fa-pause-circle">
+              <i onClick={this.togglePause} className="fa fa-pause-circle">
                 <img src="/pause.png" />
               </i>
             )}
-            <i className="fa fa-step-forward">
+            <i onClick={this.toggleSkip} className="fa fa-step-forward">
               <img src="/forward.png" />
             </i>
           </div>
@@ -53,7 +113,7 @@ export class Controller extends Component {
             type="range"
             min="1"
             max="100"
-            onInput={this.onInput.bind(this)}
+            onInput={this.seekTrack}
             defaultValue={0}
             className="slider"
             id="myRange"
@@ -63,3 +123,15 @@ export class Controller extends Component {
     )
   }
 }
+
+// const mapDispatchToProps = dispatch => {}
+
+const mapStateToProps = state => {
+  return {
+    currentTrack: state.currentTrack,
+    playlist: state.playlist,
+    player: state.player
+  }
+}
+
+export const ConnectedController = connect(mapStateToProps, null)(Controller)
